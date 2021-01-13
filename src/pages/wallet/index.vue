85362cb1fad2417f20a49c0e20b11f51e765d5fd
DevSchmidtchen <DevSchmidtchen@users.noreply.github.com>
2021-01-13 14:08:07 +0000

    npm run lint

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,8 +1,6 @@
 <template>
 	<div>
-		<h1 class="mb--md h3">
-			Digitales Wallet
-		</h1>
+		<h1 class="mb--md h3">Digitales Wallet</h1>
 		<div>
 			{{ res }}
 		</div>
@@ -12,26 +10,29 @@
 <script>
 export default {
 	async fetch() {
-		this.res = await fetch('https://daad.idas.solutions/api/v1/RelationshipTemplates', {
-			mode: 'no-cors',
-			headers: {
-				'X-API-KEY': process.env.IDAS_API_KEY
+		this.res = await fetch(
+			"https://daad.idas.solutions/api/v1/RelationshipTemplates",
+			{
+				mode: "no-cors",
+				headers: {
+					"X-API-KEY": process.env.IDAS_API_KEY,
+				},
 			}
-		}).catch((error) => {
-			console.log(error)
-		})
+		).catch((error) => {
+			console.log(error);
+		});
 
-		console.log(this.res)
+		console.log(this.res);
 	},
 	data() {
 		return {
-			res: null
-		}
+			res: null,
+		};
 	},
 	head() {
 		return {
-			title: 'Digitales Wallet'
-		}
-	}
-}
+			title: "Digitales Wallet",
+		};
+	},
+};
 </script>
