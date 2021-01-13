d1084df0a8d00dc236c6fc2ed43c824c8ab0f05c
Matti <contact@schmidtchen.me>
2021-01-13 09:43:28 +0100

    created page

---
+++ src/pages/wallet/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h1 class="mb--md h3">
+			Digitales Wallet
+		</h1>
+		<div>
+			{{ res }}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	async fetch() {
+		this.res = await fetch('https://daad.idas.solutions/api/v1/RelationshipTemplates', {
+			mode: 'no-cors',
+			headers: {
+				'X-API-KEY': process.env.IDAS_API_KEY
+			}
+		}).catch((error) => {
+			console.log(error)
+		})
+
+		console.log(this.res)
+	},
+	data() {
+		return {
+			res: null
+		}
+	},
+	head() {
+		return {
+			title: 'Digitales Wallet'
+		}
+	}
+}
+</script>
--- src/serverMiddleware/routes.js
+++ src/serverMiddleware/routes.js
@@ -34,6 +34,7 @@ const routes = [
 	// `^/news/${mongoId}/edit`,
 	`^/news/new`,
 	`^/nuxtversion/?$`,
+	`^/wallet/?$`,
 	// `^/courses/?$`,
 ];
 if (process.env.LERNSTORE_MODE === "EDUSHARING") {
