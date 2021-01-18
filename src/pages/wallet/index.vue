4a284a2c9e30eded031e8f0923079f5bf70bc2f1
Matti <contact@schmidtchen.me>
2021-01-18 19:12:48 +0100

    some implementation

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,8 +1,9 @@
 <template>
 	<div>
 		<h1 class="mb--md h3">Digitales Wallet</h1>
-		<div>
-			{{ res }}
+    <base-button design="primary" @click="connectWallet"><base-icon source="material" icon="add"/>Wallet verbinden</base-button>
+		<div v-if="qrcode">
+      <img :src="qrcode">
 		</div>
 	</div>
 </template>
@@ -10,17 +11,22 @@
 <script>
 export default {
 	async fetch() {
-		this.res = await this.$store.dispatch("wallet/find");
+
 	},
 	data() {
 		return {
-			res: null,
+			qrcode: null,
 		};
 	},
+  methods: {
+	  async connectWallet() {
+      this.qrcode = "data:image/png;base64," + await this.$store.dispatch("wallet/create");
+    }
+  },
 	head() {
 		return {
 			title: "Digitales Wallet",
 		};
-	},
+	}
 };
 </script>
