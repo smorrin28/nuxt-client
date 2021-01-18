2ed8eeb5b332e41e5ae920e01dc0888dbc9e7951
DevSchmidtchen <DevSchmidtchen@users.noreply.github.com>
2021-01-18 18:15:26 +0000

    npm run lint

--- src/pages/wallet/index.vue
+++ src/pages/wallet/index.vue
@@ -1,32 +1,34 @@
 <template>
 	<div>
 		<h1 class="mb--md h3">Digitales Wallet</h1>
-    <base-button design="primary" @click="connectWallet"><base-icon source="material" icon="add"/>Wallet verbinden</base-button>
+		<base-button design="primary" @click="connectWallet"
+			><base-icon source="material" icon="add" />Wallet verbinden</base-button
+		>
 		<div v-if="qrcode">
-      <img :src="qrcode">
+			<img :src="qrcode" />
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	async fetch() {
-
-	},
+	async fetch() {},
 	data() {
 		return {
 			qrcode: null,
 		};
 	},
-  methods: {
-	  async connectWallet() {
-      this.qrcode = "data:image/png;base64," + await this.$store.dispatch("wallet/create");
-    }
-  },
+	methods: {
+		async connectWallet() {
+			this.qrcode =
+				"data:image/png;base64," +
+				(await this.$store.dispatch("wallet/create"));
+		},
+	},
 	head() {
 		return {
 			title: "Digitales Wallet",
 		};
-	}
+	},
 };
 </script>
